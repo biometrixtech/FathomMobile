f4703ea525514eeaaf367ffbc91c0c7532301116
Vir Desai <vdesai@Virs-MacBook-Pro.local>
2017-06-29 03:32:59 +0000

    splitting roles out into a constants package and adding render views for capture session that are split by user role to show different things if needed

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,5 +5,6 @@
 import AppConfig from './config';
 import APIConfig from './api';
 import ErrorMessages from './errors';
+import Roles from './roles';
 
-export { AppConfig, APIConfig, ErrorMessages };
+export { AppConfig, APIConfig, ErrorMessages, Roles };
