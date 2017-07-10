062912fb764dd7e13ddb5b5bad004ebc9dec6f6c
Vir Desai <virdesai@Virs-MacBook-Pro-2.local>
2017-07-10 08:41:51 +0000

    redoing ble communications based on new fw flashed (1.3) with redux and flux methodology and updating eslint package

---
+++ src/redux/bluetooth/actions.js
@@ -0,0 +1,99 @@
+/**
+ * Bluetooth Actions
+ */
+import BleManager from 'react-native-ble-manager';
+import { BLEConfig } from '@constants';
+
+const Actions = require('../actionTypes');
+
+function write(id, data) {
+    return BleManager.write(id, BLEConfig.serviceUUID, BLEConfig.characteristicUUID, data);
+}
+
+/**
+  * Convert string to byte array
+  * eg.
+  *   'test'
+  *   [116,101,115,116]
+  */
+function convertStringToByteArray(string) {
+    return string.split('').map(char => char.charCodeAt(0));
+}
+
+function convertHex(value) {
+    return parseInt(value, 16);
+}
+
+export function checkState() {
+    return dispatch => new Promise(resolve => resolve(BleManager.checkState()))
+        .then(() => dispatch({
+            type: Actions.CHECK_STATE
+        }));
+}
+
+export function changeState(state) {
+    return dispatch => dispatch({
+        type: Actions.CHANGE_STATE,
+        data: state
+    });
+}
+
+export function enableBluetooth() {
+    return dispatch => BleManager.enableBluetooth()
+        .then(() => dispatch({
+            type: Actions.ENABLE_BLUETOOTH
+        }));
+}
+
+export function startBluetooth() {
+    return dispatch => BleManager.start({ showAlert: true })
+        .then(() => dispatch({
+            type: Actions.START_BLUETOOTH
+        }));
+}
+
+export function startScan() {
+    return dispatch => BleManager.scan([], 30, false)
+        .then(() => dispatch({
+            type: Actions.START_SCAN
+        }));
+}
+
+export function stopScan () {
+    return dispatch => BleManager.stopScan()
+        .then(() => dispatch({
+            type: Actions.STOP_SCAN
+        }));
+}
+
+export function deviceFound(data) {
+    return dispatch => dispatch({
+        type: Actions.DEVICE_FOUND,
+        data
+    });
+}
+
+export function connectToAccessory(data, {role, id}) {
+    console.log({data});
+    return dispatch => BleManager.connect(data.id)
+        .then(() => BleManager.retrieveServices(data.id))
+        // .then(() => {
+        //     let dataArray = new Array(20);
+        //     dataArray[0] = BLEConfig.commands.LOGIN;
+        //     dataArray[1] = convertHex('0x11');
+        //     dataArray[2] = BLEConfig.roles[];
+        //     return BleManager.write(data.id, );
+        // })
+        .then(() => dispatch({
+            type: Actions.CONNECT_TO_ACCESSORY,
+            data: {
+                accessoryConnected: true,
+                id:                 data.id,
+                name:               data.name
+            }
+        }))
+        .catch(err => {
+            console.log(err);
+            return err;
+        })
+}
