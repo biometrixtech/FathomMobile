0f759867fcdf9afb5ca9b119d44765827fba3aca
Vir Desai <virdesai@Virs-MacBook-Pro-2.local>
2017-07-18 14:46:24 +0000

    simplifying scripts and upgrading ready-native from 0.46.3 to 0.46.4 and upgrading react-native-elements from 0.14.0 to 0.15.0

--- android-build.sh
+++
@@ -1,7 +0,0 @@
-cd android
-./gradlew assembleRelease
-cd ..
-# cd app/build/outputs/apk
-echo "Release apk located at 'android/app/build/outputs/apk/' as app-release_#.apk"
-# adb install app-release*.apk
-# cd ../../../../../
