824686e542dfe15d5623f38fbad015a45156f528
Vir Desai <virdesai@Virs-MacBook-Pro.local>
2017-10-21 01:03:40 -0400

    updated script to do codepush releases, updated react-native-android-location-services-dialog-box from 2.2.1 to 2.3.2, upgraded tcomb-form-native from 0.6.10 to 0.6.11, upgraded eslint-plugin-import from 2.7.0 to 2.8.0, removed react-native-radial-menu from packages, moved team select from dashboard chart view to the sidemenu panel if multiple teams exist, centered tabbar active title with custom margins and underline, added h7 text style, added a startY to the d3 charts y scale function to make the returned number for axis and bars proper based on the graph height, changed chart sizing and padding based on feedback, implemented accumulated com acceleration graph, made the max for the axis for a chart based on the total possible max from any athlete or the entire team, fixed the x-axis padding, fixed coloring of the charts, fixed tick mark locations and cutoffs from the top of the axis line, shrunk the axis line weight and text markings weights to be minimal, changed the chart date changer to be arrows instead of words and added spacing to them to make them easier to click

--- src/components/ui/StackedBarChart.js
+++ src/components/ui/StackedBarChart.js
@@ -2,7 +2,7 @@
  * @Author: Vir Desai 
  * @Date: 2017-10-13 15:17:33 
  * @Last Modified by: Vir Desai
- * @Last Modified time: 2017-10-17 23:56:52
+ * @Last Modified time: 2017-10-20 15:07:59
  */
 
 /**
@@ -13,10 +13,8 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, TouchableHighlight, View } from 'react-native';
+import { ScrollView, TouchableHighlight, TouchableWithoutFeedback, View } from 'react-native';
 import Svg, { Rect, G } from 'react-native-svg';
-import { Icon } from 'react-native-elements';
-import ModalDropdown from 'react-native-modal-dropdown';
 
 // Consts and Libs
 import { AppColors, AppStyles, AppSizes } from '@theme/';
@@ -42,6 +40,7 @@ class StackedBarChart extends Component {
         width:     PropTypes.number,
         height:    PropTypes.number,
         tabOffset: PropTypes.number,
+        max:       PropTypes.number,
         margin:    PropTypes.shape({
             horizontal: PropTypes.number,
             vertical:   PropTypes.number
@@ -52,7 +51,6 @@ class StackedBarChart extends Component {
             y2: PropTypes.array, // irregular
         }),
         user:             PropTypes.object,
-        teamSelect:       PropTypes.func.isRequired,
         setStatsCategory: PropTypes.func.isRequired,
         getTeamStats:     PropTypes.func.isRequired
     }
@@ -62,6 +60,7 @@ class StackedBarChart extends Component {
         yAxis:     null,
         width:     0,
         height:    0,
+        max:       0,
         tabOffset: 0,
         margin:    {},
         data:      null,
@@ -77,7 +76,7 @@ class StackedBarChart extends Component {
 
     getColor = (value1, value2) => {
         if (!value1 && typeof value1 !== 'number') {
-            return AppColors.brand.grey;
+            return AppColors.greyText;
         }
         let colorIndex = threshold.findIndex(colorThreshold => colorThreshold.max >= value1 && colorThreshold.min <= value1);
         if (typeof value2 === 'number') {
@@ -87,30 +86,26 @@ class StackedBarChart extends Component {
         return threshold[colorIndex].color;
     } 
 
-    getTickPoints (vertical, start, end, numTicks, length) {
+    getTickPoints (start, end, numTicks, length) {
         let res = [];
         let ticksEvery = Math.floor(length / (numTicks - 1));
-        if (vertical) {
-            for (let cur = start; cur >= end; cur -= ticksEvery) { res.push(cur); }
-        } else {
-            for (let cur = start; cur <= end; cur += ticksEvery) { res.push(cur); }
-        }
+        for (let cur = start; cur <= end; cur += ticksEvery) { res.push(cur); }
         return res;
     }
 
     renderBar = (width, height, y1, y2, index) => {
-        let array = this.getTickPoints(false, 3 * this.props.margin.horizontal, width + 6 * this.props.margin.horizontal, 7, width);
+        let array = this.getTickPoints(3 * this.props.margin.horizontal, width + 6 * this.props.margin.horizontal, 7, width);
         return <G key={index}>
             <Rect
                 x={array[index] + AppSizes.tickSize+1.5}
-                y={height - (y1 + 2)}
+                y={height - (y1 + 0.5)}
                 width={8}
                 height={y1}
                 fill={AppColors.brand.fogGrey}
             />
             <Rect
                 x={array[index] + AppSizes.tickSize+1.5}
-                y={height - (y2 + 2)}
+                y={height - (y2 + 0.5)}
                 width={8}
                 height={y2}
                 fill={AppColors.brand.yellow}
@@ -118,46 +113,33 @@ class StackedBarChart extends Component {
         </G>
     }
 
-    modalDropdown = () => {
-        return <View style={{ flex: 1, backgroundColor: '#FFFFFF', alignSelf: 'center' }} >
-            <View style={{ justifyContent: 'center', flexDirection: 'row' }}>
-                <ModalDropdown
-                    options={this.props.user.teams.map(team => team.name)}
-                    defaultIndex={this.props.user.teamIndex}
-                    defaultValue={this.props.user.teams[this.props.user.teamIndex].name}
-                    textStyle={AppStyles.h3}
-                    dropdownTextStyle={AppStyles.h3}
-                    onSelect={index =>  Promise.resolve(this.props.teamSelect(index))}
-                />
-                <Icon name={'caret-down'} type={'font-awesome'} size={16} containerStyle={{ marginLeft: 5 }} color={AppColors.brand.blue}/>
-            </View>
-        </View>
-    }
-
     render = () => {
-        let {xAxis, yAxis, width, height, margin, data, tabOffset, user} = this.props;
+        let {xAxis, yAxis, width, height, margin, data, tabOffset, user, max} = this.props;
 
         let startDateComponents = user.statsStartDate ? user.statsStartDate.split('-') : (new Date()).toLocaleDateString().split('/');
         let endDateComponents = user.statsEndDate ? user.statsEndDate.split('-') : (new Date()).toLocaleDateString().split('/');
-        let maxInData = data ? Math.round(Math.max(...data.y1)) : 0;
-        let maxInDataLength = maxInData.toString().length;
-        let roundTo = maxInDataLength === 1 ? 1 : maxInDataLength === 2 ? 10 : maxInDataLength === 3 ? 100 : 1000;
-        let maxRange = Math.round(maxInData/roundTo) * roundTo + roundTo;
         let xScale = data ? data.x[0] instanceof Date ? AppUtil.createTimeScaleX(data.x[0], data.x[data.x.length - 1], width - 2 * margin.horizontal) : AppUtil.createScaleX(data.x[0], data.x[data.x.length - 1], width - 2 * margin.horizontal) : null;
-        let yScale = AppUtil.createScaleY(0, maxRange, height - 2 * margin.vertical);
+        let yScale = AppUtil.createScaleY(0, max, height - 2 * margin.vertical, margin.vertical);
+
+        console.log(data);
 
         return (
             <View>
-                <Spacer size={5}/>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }} onLayout={ev => this.setState({ chartHeaderHeight: ev.nativeEvent.layout.height })}>
-                    <Text onPress={() => this.props.getTeamStats(user.teams[user.teamIndex].id, user.weekOffset-1)}>Prev</Text>
-                    <Text>{`${startDateComponents[1]}/${startDateComponents[2]}/${startDateComponents[0].substring(2)}`}-{`${endDateComponents[1]}/${endDateComponents[2]}/${endDateComponents[0].substring(2)}`}</Text>
-                    <Text onPress={() => this.props.getTeamStats(user.teams[user.teamIndex].id, user.weekOffset+1)}>Next</Text>
+                <View style={{ flexDirection: 'row' }} onLayout={ev => this.setState({ chartHeaderHeight: ev.nativeEvent.layout.height })}>
+                    <TouchableWithoutFeedback onPress={() => this.props.getTeamStats(user.teams[user.teamIndex].id, user.weekOffset-1)}>
+                        <View style={[AppStyles.containerCentered, { flex: 1 }]}><Spacer /><Text h3>{'<'}</Text><Spacer /></View>
+                    </TouchableWithoutFeedback>
+                    <View style={[AppStyles.containerCentered, { flex: 2 }]}>
+                        <Text>{`${startDateComponents[1]}/${startDateComponents[2]}/${startDateComponents[0].substring(2)}`}-{`${endDateComponents[1]}/${endDateComponents[2]}/${endDateComponents[0].substring(2)}`}</Text>
+                    </View>
+                    <TouchableWithoutFeedback onPress={() => this.props.getTeamStats(user.teams[user.teamIndex].id, user.weekOffset+1)}>
+                        <View style={[AppStyles.containerCentered, { flex: 1 }]}><Spacer /><Text h3>{'>'}</Text><Spacer /></View>
+                    </TouchableWithoutFeedback>
                 </View>
                 {
                     !user.teams[user.teamIndex].stats ? <View style={{ alignSelf: 'center' }}><Placeholder text={'No data to show for this range...'} /></View> :
                         <View>
-                            { xAxis ? <Text style={[AppStyles.subtext, { position: 'absolute', left: -4 * margin.horizontal - tabOffset * AppSizes.tickSize, top: height*9/20, transform: [{ rotate: '270deg' }] }]}>{xAxis}</Text> : null }
+                            { xAxis ? <Text style={[AppStyles.h7, { position: 'absolute', left: -4 * margin.horizontal - tabOffset * AppSizes.tickSize, top: height*9/20, transform: [{ rotate: '270deg' }] }]}>{xAxis}</Text> : null }
                             { yAxis ? <Text style={[AppStyles.subtext, { position: 'absolute', left: width/2, top: height + margin.vertical }]}>{yAxis}</Text> : null }
                             <Svg width={width + 3 * margin.horizontal} height={height}>
                                 <Axis
@@ -172,15 +154,16 @@ class StackedBarChart extends Component {
                                     length={height - 3 * margin.vertical}
                                     x={3 * margin.horizontal}
                                     y={height - 2 * margin.vertical}
-                                    ticks={10}
+                                    ticks={11}
                                     startVal={0}
-                                    endVal={maxRange}
+                                    endVal={max}
                                     scale={yScale}
                                     vertical />
                                 {
-                                    data.x.map((xValues, index) => this.renderBar(width - margin.horizontal, height - 2 * margin.vertical, yScale(maxRange-data.y1[index]), yScale(maxRange-data.y2[index]), index))
+                                    data.x.map((xValues, index) => this.renderBar(width - margin.horizontal, height - 2 * margin.vertical, yScale(max-data.y1[index])-yScale(max), yScale(max-data.y2[index])-yScale(max), index))
                                 }
                             </Svg>
+                            <Spacer size={20}/>
                             <View style={{ flexDirection: 'row' }} onLayout={ev => this.setState({ listHeaderHeight: ev.nativeEvent.layout.height })}>
                                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around' }}>
                                     <View style={{ alignItems: 'center' }}>
@@ -192,14 +175,14 @@ class StackedBarChart extends Component {
                                         <Text style={AppStyles.subtext}>DAILY AVG</Text>
                                     </View>
                                 </View>
-                                { this.modalDropdown() }
+                                <View style={{ flex: 1 }}/>
                             </View>
-                            <ScrollView style={{ backgroundColor: AppColors.brand.light, height: AppSizes.screen.usableHeight - (this.props.height + this.state.chartHeaderHeight + this.state.listHeaderHeight) }} scrollEnabled={true} contentContainerStyle={{ height: (user.teams[user.teamIndex].stats && user.teams[user.teamIndex].stats.AthleteMovementQualityData ? user.teams[user.teamIndex].stats.AthleteMovementQualityData.length + 1 : 0) * 55 }}>
+                            <ScrollView style={{ backgroundColor: AppColors.brand.light, height: AppSizes.screen.usableHeight - (this.props.height + this.state.chartHeaderHeight + this.state.listHeaderHeight + 10) }} scrollEnabled={true} contentContainerStyle={{ height: (user.teams[user.teamIndex].stats && user.teams[user.teamIndex].stats.AthleteMovementQualityData ? user.teams[user.teamIndex].stats.AthleteMovementQualityData.length + 1 : 0) * 55 }}>
                                 <TouchableHighlight key={-1} onPress={() => this.props.setStatsCategory(false, null)}>
                                     <View style={{ flexDirection: 'row', marginTop: 2, marginBottom: 2, backgroundColor: user.selectedStats.athlete ? 'white' : AppColors.lightGrey }}>
                                         <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', paddingTop: AppSizes.paddingSml, paddingBottom: AppSizes.paddingSml }}>
                                             <Text style={[AppStyles.subtext, { color: this.getColor(user.teams[user.teamIndex].stats && user.teams[user.teamIndex].stats.acwrGRF7) }]}>{user.teams[user.teamIndex].stats ? user.teams[user.teamIndex].stats.acwrGRF7 : ''}</Text>
-                                            <Text style={[AppStyles.subtext]}>{user.teams[user.teamIndex].stats ? user.teams[user.teamIndex].stats.chronicGRF : ''}</Text>
+                                            <Text style={[AppStyles.subtext, { color: AppColors.greyText }]}>{user.teams[user.teamIndex].stats ? user.teams[user.teamIndex].stats.chronicGRF : ''}</Text>
                                         </View>
                                         <View style={[AppStyles.containerCentered, { flex: 1 }]}>
                                             <Text style={{ color: this.getColor(user.teams[user.teamIndex].stats && user.teams[user.teamIndex].stats.acwrGRF7) }}>Team Avg</Text>
@@ -215,7 +198,7 @@ class StackedBarChart extends Component {
                                                     <View style={{ flexDirection: 'row', marginTop: 2, marginBottom: 2, backgroundColor: user.selectedStats.athlete && user.selectedStats.athleteId === athlete.id ? AppColors.lightGrey : 'white' }}>
                                                         <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', paddingTop: AppSizes.paddingSml, paddingBottom: AppSizes.paddingSml }}>
                                                             <Text style={[AppStyles.subtext, { color: this.getColor(athleteMovementQualityData.acwrGRF7) }]}>{athleteMovementQualityData.acwrGRF7}</Text>
-                                                            <Text style={[AppStyles.subtext]}>{athleteMovementQualityData.chronicGRF}</Text>
+                                                            <Text style={[AppStyles.subtext, { color: AppColors.greyText }]}>{athleteMovementQualityData.chronicGRF}</Text>
                                                         </View>
                                                         <View style={[AppStyles.containerCentered, { flex: 1 }]}>
                                                             <Text style={{ color: this.getColor(athleteMovementQualityData.acwrGRF7) }}>{athlete.first_name} {athlete.last_name}</Text>
