0fd4b501dca2e59043d40694d6422c5d43ede36e
Vir Desai <vir.desai.nc@gmail.com>
2017-10-21 01:03:40 -0400

    updated script to do codepush releases, updated react-native-android-location-services-dialog-box from 2.2.1 to 2.3.2, upgraded tcomb-form-native from 0.6.10 to 0.6.11, upgraded eslint-plugin-import from 2.7.0 to 2.8.0, removed react-native-radial-menu from packages, moved team select from dashboard chart view to the sidemenu panel if multiple teams exist, centered tabbar active title with custom margins and underline, added h7 text style, added a startY to the d3 charts y scale function to make the returned number for axis and bars proper based on the graph height, changed chart sizing and padding based on feedback, implemented accumulated com acceleration graph, made the max for the axis for a chart based on the total possible max from any athlete or the entire team, fixed the x-axis padding, fixed coloring of the charts, fixed tick mark locations and cutoffs from the top of the axis line, shrunk the axis line weight and text markings weights to be minimal, changed the chart date changer to be arrows instead of words and added spacing to them to make them easier to click

--- src/components/ui/Text.js
+++ src/components/ui/Text.js
@@ -2,7 +2,7 @@
  * @Author: Vir Desai 
  * @Date: 2017-10-12 11:30:36 
  * @Last Modified by: Vir Desai
- * @Last Modified time: 2017-10-17 20:14:27
+ * @Last Modified time: 2017-10-18 22:51:24
  */
 
 /**
@@ -27,6 +27,7 @@ class CustomText extends Component {
         h4:      PropTypes.bool,
         h5:      PropTypes.bool,
         h6:      PropTypes.bool,
+        h7:      PropTypes.bool,
         p:       PropTypes.bool,
         onPress: PropTypes.func,
         style:   PropTypes.oneOfType([
@@ -43,6 +44,7 @@ class CustomText extends Component {
         h4:       false,
         h5:       false,
         h6:       false,
+        h7:       false,
         p:        false,
         onPress:  null,
         style:    null,
@@ -56,13 +58,14 @@ class CustomText extends Component {
             style: [AppStyles.baseText],
         };
 
-        if (this.props.p) { props.style = [AppStyles.p]; }
+        if (this.props.p)  { props.style = [AppStyles.p];  }
         if (this.props.h1) { props.style = [AppStyles.h1]; }
         if (this.props.h2) { props.style = [AppStyles.h2]; }
         if (this.props.h3) { props.style = [AppStyles.h3]; }
         if (this.props.h4) { props.style = [AppStyles.h4]; }
         if (this.props.h5) { props.style = [AppStyles.h5]; }
         if (this.props.h6) { props.style = [AppStyles.h6]; }
+        if (this.props.h7) { props.style = [AppStyles.h7]; }
         if (this.props.onPress) { props.style.push(AppStyles.link); }
 
         if (this.props.style) {
