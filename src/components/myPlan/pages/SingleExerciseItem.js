d723f4591fa0af21e0dbe1d79ee82fa9422907f4
Mazen Chami <mazenchami@gmail.com>
2018-08-07 13:19:38 -0400

    removing comments we dont need anymore

--- src/components/myPlan/pages/SingleExerciseItem.js
+++ src/components/myPlan/pages/SingleExerciseItem.js
@@ -10,7 +10,7 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, Platform, View, WebView } from 'react-native';
+import { View, } from 'react-native';
 
 // import third-party libraries
 import Video from 'react-native-video';
@@ -57,25 +57,6 @@ const SingleExerciseItem = ({
         />
     </View>
 );
-
-/*
-<View style={{ height: 200 }}>
-    <WebView
-        allowsInlineMediaPlayback={true}
-        automaticallyAdjustContentInsets={false}
-        domStorageEnabled={true}
-        javaScriptEnabled={true}
-        mediaPlaybackRequiresUserAction={false}
-        originWhitelist={['*']}
-        scalesPageToFit={true}
-        scrollEnabled={false}
-        source={{ uri: exercise.youtubeId }}
-        startInLoadingState={true}
-        style={{flex: 1,}}
-    />
-</View>
-
-*/
 
 SingleExerciseItem.propTypes = {
     exercise:               PropTypes.object.isRequired,
