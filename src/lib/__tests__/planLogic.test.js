29f9b0b7f025583cc97365c3a15014c9c2db2bcf
Mazen Chami <mazenchami@gmail.com>
2018-10-19 11:54:34 -0400

    updates to _handlePushNotification tests

--- src/lib/__tests__/planLogic.test.js
+++ src/lib/__tests__/planLogic.test.js
@@ -4,54 +4,128 @@ import 'react-native';
 import { PlanLogic, } from '../';
 
 const helperFunctions = {
-    pushNotification: (notificationString) => {
-        let helperProps = {};
-        helperProps.notification = notificationString;
-        helperProps.plan = {};
-        helperProps.plan.dailyPlan = [];
-        helperProps.plan.dailyPlan[0] = {};
-        helperProps.plan.dailyPlan[0].pre_recovery_completed = false;
-        helperProps.plan.dailyPlan[0].post_recovery = {};
-        helperProps.plan.dailyPlan[0].post_recovery.completed = false;
-        helperProps.plan.dailyPlan[0].daily_readiness_survey_completed = false;
-        let helperState = {};
-        helperState.prepare = {
+
+    getPushNotificationHelperProps: (notificationString) => {
+        let pushNotificationHelperProps = {};
+        pushNotificationHelperProps.notification = notificationString;
+        pushNotificationHelperProps.plan = {};
+        pushNotificationHelperProps.plan.dailyPlan = [];
+        pushNotificationHelperProps.plan.dailyPlan[0] = {};
+        pushNotificationHelperProps.plan.dailyPlan[0].pre_recovery_completed = false;
+        pushNotificationHelperProps.plan.dailyPlan[0].post_recovery = {};
+        pushNotificationHelperProps.plan.dailyPlan[0].post_recovery.completed = false;
+        pushNotificationHelperProps.plan.dailyPlan[0].daily_readiness_survey_completed = false;
+        return pushNotificationHelperProps;
+    },
+
+    getPushNotificationHelperState: (notificationString, helperProps) => {
+        let pushNotificationHelperState = {};
+        pushNotificationHelperState.prepare = {
             finishedRecovery:           helperProps.plan && helperProps.plan.dailyPlan[0] && helperProps.plan.dailyPlan[0].pre_recovery_completed ? true : false,
             isActiveRecoveryCollapsed:  true,
             isReadinessSurveyCollapsed: true,
             isReadinessSurveyCompleted: false,
         };
-        helperState.recover = {
+        pushNotificationHelperState.recover = {
             finished:                  false,
             isActiveRecoveryCollapsed: true,
         };
+        return pushNotificationHelperState;
+    },
+
+    getPushNotificationAPExpectedResult: () => {
         let expectedResult = {
-            newStateFields: notificationString === 'COMPLETE_ACTIVE_PREP' ?
-                {
-                    finishedRecovery:           false,
-                    isActiveRecoveryCollapsed:  false,
-                    isReadinessSurveyCollapsed: true,
-                    isReadinessSurveyCompleted: false
-                }
-                : notificationString === 'COMPLETE_ACTIVE_RECOVERY' ?
-                    {finished: false, isActiveRecoveryCollapsed: false}
-                    : notificationString === 'COMPLETE_DAILY_READINESS' ?
-                        true
-                        :
-                        '',
-            page:                       notificationString === 'COMPLETE_ACTIVE_RECOVERY' ? 2 : 0,
-            stateName:                  notificationString === 'COMPLETE_ACTIVE_PREP' ? 'prepare' : notificationString === 'COMPLETE_ACTIVE_RECOVERY' ? 'recover' : notificationString === 'COMPLETE_DAILY_READINESS' ? 'isReadinessSurveyModalOpen' : '',
-            updateExerciseList:         notificationString === 'VIEW_PLAN' || notificationString === 'FATHOM_BREAK' ? true : false,
+            newStateFields: {
+                finishedRecovery:           false,
+                isActiveRecoveryCollapsed:  false,
+                isReadinessSurveyCollapsed: true,
+                isReadinessSurveyCompleted: false
+            },
+            page:                       0,
+            stateName:                  'prepare',
+            updateExerciseList:         false,
             updatePushNotificationFlag: true
         };
-        return { expectedResult, helperProps, helperState, };
+        return expectedResult;
     },
+
+    getPushNotificationARExpectedResult: () => {
+        let expectedResult = {
+            newStateFields:             { finished: false, isActiveRecoveryCollapsed: false },
+            page:                       2,
+            stateName:                  'recover',
+            updateExerciseList:         false,
+            updatePushNotificationFlag: true
+        };
+        return expectedResult;
+    },
+
+    getPushNotificationReadinessExpectedResult: () => {
+        let expectedResult = {
+            newStateFields:             true,
+            page:                       0,
+            stateName:                  'isReadinessSurveyModalOpen',
+            updateExerciseList:         false,
+            updatePushNotificationFlag: true
+        };
+        return expectedResult;
+    },
+
+    getPushNotificationPlanExpectedResult: () => {
+        let expectedResult = {
+            newStateFields:             '',
+            page:                       0,
+            stateName:                  '',
+            updateExerciseList:         true,
+            updatePushNotificationFlag: true
+        };
+        return expectedResult;
+    },
+
+    getPushNotificationExtraExpectedResult: () => {
+        let expectedResult = {
+            newStateFields:             '',
+            page:                       0,
+            stateName:                  '',
+            updateExerciseList:         true,
+            updatePushNotificationFlag: true
+        };
+        return expectedResult;
+    },
+
 };
 
 it('handlePushNotification', () => {
-    const validNotifs = ['COMPLETE_ACTIVE_PREP', 'COMPLETE_ACTIVE_RECOVERY', 'COMPLETE_DAILY_READINESS', 'VIEW_PLAN', 'FATHOM_BREAK'];
-    for (let i = 0; i < validNotifs.length; i+=1) {
-        let { expectedResult, helperProps, helperState, } = helperFunctions.pushNotification(validNotifs[i]);
-        expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
-    }
+    let helperProps = helperFunctions.getPushNotificationHelperProps('COMPLETE_ACTIVE_PREP');
+    let helperState = helperFunctions.getPushNotificationHelperState('COMPLETE_ACTIVE_PREP', helperProps);
+    let expectedResult = helperFunctions.getPushNotificationAPExpectedResult();
+    expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
+});
+
+it('handlePushNotification', () => {
+    let helperProps = helperFunctions.getPushNotificationHelperProps('COMPLETE_ACTIVE_RECOVERY');
+    let helperState = helperFunctions.getPushNotificationHelperState('COMPLETE_ACTIVE_RECOVERY', helperProps);
+    let expectedResult = helperFunctions.getPushNotificationARExpectedResult();
+    expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
+});
+
+it('handlePushNotification', () => {
+    let helperProps = helperFunctions.getPushNotificationHelperProps('COMPLETE_DAILY_READINESS');
+    let helperState = helperFunctions.getPushNotificationHelperState('COMPLETE_DAILY_READINESS', helperProps);
+    let expectedResult = helperFunctions.getPushNotificationReadinessExpectedResult();
+    expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
+});
+
+it('handlePushNotification', () => {
+    let helperProps = helperFunctions.getPushNotificationHelperProps('VIEW_PLAN');
+    let helperState = helperFunctions.getPushNotificationHelperState('VIEW_PLAN', helperProps);
+    let expectedResult = helperFunctions.getPushNotificationPlanExpectedResult();
+    expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
+});
+
+it('handlePushNotification', () => {
+    let helperProps = helperFunctions.getPushNotificationHelperProps('FATHOM_BREAK');
+    let helperState = helperFunctions.getPushNotificationHelperState('FATHOM_BREAK', helperProps);
+    let expectedResult = helperFunctions.getPushNotificationExtraExpectedResult();
+    expect(PlanLogic.handlePushNotification(helperProps, helperState)).toEqual(expectedResult);
 });
--- src/lib/planLogic.js
+++ src/lib/planLogic.js
@@ -13,7 +13,7 @@ const PlanLogic = {
         // setup varibles
         let dailyPlan = props.plan.dailyPlan[0];
         const validNotifs = ['COMPLETE_ACTIVE_PREP', 'COMPLETE_ACTIVE_RECOVERY', 'COMPLETE_DAILY_READINESS', 'VIEW_PLAN',];
-        let returnObj = {
+        let pushNotificationResults = {
             newStateFields:             '',
             page:                       0,
             stateName:                  '',
@@ -23,27 +23,27 @@ const PlanLogic = {
         // logic
         if(props.notification === 'COMPLETE_ACTIVE_PREP' && !dailyPlan.pre_recovery_completed) {
             // go to screen 0 & open active prep
-            returnObj.newStateFields = _.update( state.prepare, 'isActiveRecoveryCollapsed', () => false);
-            returnObj.stateName = 'prepare';
-            returnObj.updatePushNotificationFlag = true;
+            pushNotificationResults.newStateFields = _.update( state.prepare, 'isActiveRecoveryCollapsed', () => false);
+            pushNotificationResults.stateName = 'prepare';
+            pushNotificationResults.updatePushNotificationFlag = true;
         } else if(props.notification === 'COMPLETE_ACTIVE_RECOVERY' && !dailyPlan.post_recovery.completed) {
             // go to screen 2 & open active recovery
-            returnObj.page = 2;
-            returnObj.newStateFields = _.update( state.recover, 'isActiveRecoveryCollapsed', () => false);
-            returnObj.stateName = 'recover';
-            returnObj.updatePushNotificationFlag = true;
+            pushNotificationResults.page = 2;
+            pushNotificationResults.newStateFields = _.update( state.recover, 'isActiveRecoveryCollapsed', () => false);
+            pushNotificationResults.stateName = 'recover';
+            pushNotificationResults.updatePushNotificationFlag = true;
         } else if(props.notification === 'COMPLETE_DAILY_READINESS' && !dailyPlan.daily_readiness_survey_completed) {
             // go to screen 0 & open daily_readiness
-            returnObj.newStateFields = true;
-            returnObj.stateName = 'isReadinessSurveyModalOpen';
-            returnObj.updatePushNotificationFlag = true;
+            pushNotificationResults.newStateFields = true;
+            pushNotificationResults.stateName = 'isReadinessSurveyModalOpen';
+            pushNotificationResults.updatePushNotificationFlag = true;
         } else if(props.notification === 'VIEW_PLAN' || !validNotifs.includes(props.notification)) {
             // added catch in case of view plan or other message, do what we did in the past
-            returnObj.updateExerciseList = true;
-            returnObj.updatePushNotificationFlag = true;
+            pushNotificationResults.updateExerciseList = true;
+            pushNotificationResults.updatePushNotificationFlag = true;
         }
         // return
-        return returnObj;
+        return pushNotificationResults;
     },
 
 };
