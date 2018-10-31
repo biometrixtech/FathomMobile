d640101f8f91484ea297dff874d7b16707eef1c2
Mazen Chami <mazenchami@gmail.com>
2018-10-31 15:18:23 -0400

    moving console logs

--- src/components/myPlan/MyPlan.js
+++ src/components/myPlan/MyPlan.js
@@ -409,12 +409,7 @@ class MyPlan extends Component {
 
     _handleAreaOfSorenessClick = (areaClicked, isDailyReadiness, isAllGood) => {
         let stateObject = isDailyReadiness ? this.state.dailyReadiness : this.state.postSession;
-        console.log('stateObject',stateObject);
-        console.log('areaClicked',areaClicked);
-        console.log('isAllGood',isAllGood);
-        console.log('this.props.plan.soreBodyParts',this.props.plan.soreBodyParts);
         let newSorenessFields = PlanLogic.handleAreaOfSorenessClick(stateObject, areaClicked, isAllGood, this.props.plan.soreBodyParts);
-        console.log('newSorenessFields',newSorenessFields);
         let newFormFields = _.update( stateObject, 'soreness', () => newSorenessFields);
         if (isDailyReadiness) {
             this.setState({
