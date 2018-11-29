a6ca378ba0f24945c1777a85443f5dabff26ddbf
Mazen Chami <mazenchami@gmail.com>
2018-11-29 14:20:09 -0500

    coaches dashboard refresh button & version bump

--- src/components/custom/CustomNavBar.js
+++ src/components/custom/CustomNavBar.js
@@ -6,12 +6,14 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Image, Platform, StatusBar, StyleSheet, View, } from 'react-native';
+import { Animated, Easing, Image, Platform, StatusBar, StyleSheet, View, } from 'react-native';
+import { connect } from 'react-redux';
 
 // Consts and Libs
-import { AppColors, AppSizes, AppStyles, } from '../../constants';
+import { AppColors, AppSizes, AppStyles, ErrorMessages, } from '../../constants';
+import { AppUtil, } from '../../lib';
+import { plan as PlanActions, } from '../../actions';
 import { TabIcon, Text, } from './';
-import { store } from '../../store';
 
 // import third-party libraries
 import { Actions, } from 'react-native-router-flux';
@@ -33,6 +35,9 @@ class CustomNavBar extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isCoachesDashboardLoading: false,
+        }
     }
 
     componentDidMount = () => {
@@ -45,7 +50,7 @@ class CustomNavBar extends Component {
     _renderLeft = () => {
         return (
             <View style={{flex: 1, justifyContent: 'center', paddingLeft: AppSizes.paddingXSml,}}>
-                { this.props.routeName === 'onboarding' && !store.getState().user.id ?
+                { this.props.routeName === 'onboarding' && !this.props.user.id ?
                     <TabIcon
                         icon={'arrow-left'}
                         iconStyle={[{color: AppColors.black,}]}
@@ -96,7 +101,59 @@ class CustomNavBar extends Component {
     }
 
     _renderRight = () => {
+        // set animated values
+        const spinValue = new Animated.Value(0);
+        // First set up animation
+        Animated.loop(
+            Animated.timing(
+                spinValue,
+                {
+                    duration:        3000,
+                    easing:          Easing.linear,
+                    toValue:         1,
+                    useNativeDriver: true,
+                }
+            )
+        ).start();
+        // Second interpolate beginning and end values (in this case 0 and 1)
+        const spin = spinValue.interpolate({
+            inputRange:  [0, 1],
+            outputRange: ['0deg', '360deg'],
+        });
+        if(this.props.routeName === 'coachesDashboard') {
+            return(
+                <Animated.View
+                    style={{
+                        flex:           1,
+                        justifyContent: 'center',
+                        paddingLeft:    AppSizes.paddingXSml,
+                        transform:      this.state.isCoachesDashboardLoading ? [{rotate: spin}] : [],
+                    }}
+                >
+                    <TabIcon
+                        icon={'cached'}
+                        iconStyle={[{color: AppColors.black,}]}
+                        onPress={() => this._handleCoachesDashboardRefresh()}
+                        reverse={false}
+                        size={26}
+                        type={'material-community'}
+                    />
+                </Animated.View>
+            )
+        }
         return(<View style={{flex: 1}}></View>)
+    }
+
+    _handleCoachesDashboardRefresh = () => {
+        // fetch coaches dashboard data
+        let userId = this.props.user.id;
+        this.setState({ isCoachesDashboardLoading: true, });
+        this.props.getCoachesDashboardData(userId)
+            .then(res => this.setState({ isCoachesDashboardLoading: false, }))
+            .catch(err => {
+                this.setState({ isCoachesDashboardLoading: false, });
+                AppUtil.handleAPIErrorAlert(ErrorMessages.patchFunctionalStrength);
+            });
     }
 
     render = () => {
@@ -115,4 +172,12 @@ class CustomNavBar extends Component {
 }
 
 /* Export Component ==================================================================== */
-export default CustomNavBar;
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+const mapDispatchToProps = {
+    getCoachesDashboardData: PlanActions.getCoachesDashboardData,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomNavBar);
