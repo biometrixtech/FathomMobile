a02f651cdc4781646d65b864ae2dafbec4f4ac96
Mazen Chami <mazenchami@gmail.com>
2019-01-18 09:00:48 -0500

    trimming email address to help with invalid emails coming up

--- src/constants/utils/onboardingUtils.js
+++ src/constants/utils/onboardingUtils.js
@@ -133,7 +133,7 @@ const onboardingUtils = {
 
     isEmailValid(email) {
         const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        let isValid = emailRegex.test(email);
+        let isValid = emailRegex.test(email.trim());
         let errorsArray = []
         if(!isValid)
         {
