9055c26272e7188684b5fd961e79a3693fe0e07c
Mazen Chami <mazenchami@gmail.com>
2019-03-28 16:39:53 -0400

    adding all libraries and linking where needed

---
+++ android/app/src/main/java/com/fathom/MainApplication.java
@@ -0,0 +1,71 @@
+package com.fathom;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.smixx.fabric.FabricPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
+import com.corbt.keepawake.KCKeepAwakePackage;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import it.innove.BleManagerPackage;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
+import com.airbnb.android.react.lottie.LottiePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new FabricPackage(),
+            new SplashScreenReactPackage(),
+            new VectorIconsPackage(),
+            new ReactVideoPackage(),
+            new ReactNativePushNotificationPackage(),
+            new LinearGradientPackage(),
+            new GoogleAnalyticsBridgePackage(),
+            new KCKeepAwakePackage(),
+            new RNSoundPackage(),
+            new RNDeviceInfo(),
+            new BleManagerPackage(),
+            new LocationServicesDialogBoxPackage(),
+            new LottiePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
